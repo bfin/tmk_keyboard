516a7ca7218d39313d157ac05df99915d81c00a5
Brian Findlay <bfindlay@fastmail.com>
2018-08-06 15:18:36 -0400

    Switch Layer-2 tap key from semicolon to spacebar

--- keyboard/hhkb/unimap_bfin.c
+++ keyboard/hhkb/unimap_bfin.c
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #define AC_L1       ACTION_LAYER_TAP_TOGGLE(1)
-#define AC_L2       ACTION_LAYER_TAP_KEY(2, KC_SCLN)
+#define AC_L2       ACTION_LAYER_TAP_KEY(2, KC_SPC)
 #define AC_CTL_     ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC)
 
 
@@ -30,21 +30,21 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
     [0] = UNIMAP_HHKB(
     ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,
     TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,     BSPC,
-    CTL_,A,   S,   D,   F,   G,   H,   J,   K,   L,   L2,  QUOT,          ENT,
+    CTL_,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,          ENT,
     LSFT,     Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,L1,
-         LALT,LGUI,               SPC,                     RGUI,RALT),
+         LALT,LGUI,               L2,                      RGUI,RALT),
 
     [1] = UNIMAP_HHKB(
     PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL,
-    CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS,UP,  TRNS,     TRNS,
+    CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS,UP,  TRNS,     DEL,
     TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,          ENT,
-    TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,     TRNS,TRNS,
+    TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,    TRNS,TRNS,
          TRNS,TRNS,               TRNS,                    TRNS,TRNS),
 
     [2] = UNIMAP_HHKB(
     PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL,
-    CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  TRNS,     TRNS,
+    CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  TRNS,   DEL,
     TRNS,VOLD,VOLU,MUTE,MPLY,MNXT,LEFT,DOWN,  UP,RGHT,TRNS,TRNS,          ENT,
-    TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, TRNS,TRNS,     TRNS,TRNS,
+    TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, TRNS,TRNS,   TRNS,TRNS,
          TRNS,TRNS,               TRNS,                    TRNS,TRNS),
 };
